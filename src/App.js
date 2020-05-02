e62d7227f5ef10960e0b1efcee10cef2b89aa6b5
PavanS27 <48400643+PavanS27@users.noreply.github.com>
2020-05-02 08:15:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Cards, Charts, Country } from "./components";
+import styles from "./App.module.css";
+import { fetchData } from "./api";
+class App extends Component {
+  state = {
+    data: {},
+    country: "",
+  };
+  async componentDidMount() {
+    const fetchedData = await fetchData();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    this.setState({
+      data: fetchedData,
+    });
+  }
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    this.setState({
+      data: fetchedData,
+      country: country,
+    });
+  };
+
+  render() {
+    const { data, country } = this.state;
+    return (
+      <div className={styles.container}>
+        <img
+          className={styles.image}
+          alt="covid19"
+          src="https://www.nsmedicaldevices.com/wp-content/uploads/sites/2/2020/03/599px-2019-nCoV-CDC-23312_without_background-740x520.png"
+        />
+        <Cards data={data} />
+        <Country handleCountryChange={this.handleCountryChange} />
+        <Charts data={data} country={country} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Cards } from "./Cards/Cards";
+export { default as Charts } from "./Chart/Charts";
+export { default as Country } from "./CountryPicker/Country";
